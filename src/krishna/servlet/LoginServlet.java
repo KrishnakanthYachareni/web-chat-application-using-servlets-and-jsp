f09fda32cb0394ee77982a1fa1611f4bf1f7a94c
Krishnakanth Yachareni <Krishnakanth_Yachareni@epam.com>
2018-06-18 23:01:23 +0530

    converted Maven

--- src/krishna/servlet/LoginServlet.java
+++ src/krishna/servlet/LoginServlet.java
@@ -6,79 +6,66 @@ import javax.servlet.http.HttpSession;
 
 import krishna.bean.ChatRoomList;
 
+public class LoginServlet extends javax.servlet.http.HttpServlet {
+	private String contextPath;
 
-public class LoginServlet
-  extends javax.servlet.http.HttpServlet
-{
-  private String contextPath;
-  
-  public LoginServlet() { jdMethod_this(); }
-  
-  private final void jdMethod_this() { contextPath = ""; }
-  
-  public void doGet(HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse) throws javax.servlet.ServletException, java.io.IOException
-  {
-    contextPath = paramHttpServletRequest.getContextPath();
-    paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp");
-  }
-  
+	public LoginServlet() {
+		jdMethod_this();
+	}
 
-  public void doPost(HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse)
-    throws javax.servlet.ServletException, java.io.IOException
-  {
-    contextPath = paramHttpServletRequest.getContextPath();
-    String str1 = paramHttpServletRequest.getParameter("nickname");
-    str1 = str1.trim().toLowerCase();
-    if ((str1 != null) && (str1.length() > 3) && (str1.indexOf(" ") == -1))
-    {
-    	try
-        {
-          ChatRoomList localChatRoomList = (ChatRoomList)getServletContext().getAttribute("chatroomlist");
-          boolean bool = localChatRoomList.chatterExists(str1);
-          if (bool)
-          {
-            paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp?d=t&n=" + str1);
-          }
-          else
-          {
-            HttpSession localHttpSession = paramHttpServletRequest.getSession(true);
-            int i = 1800;
-            String str3 = getServletContext().getInitParameter("sessionTimeout");
-            if (str3 != null)
-            {
-              try
-              {
-                i = Integer.parseInt(str3);
-                i *= 60;
-              }
-              catch (NumberFormatException localNumberFormatException) {}
-            }
-            
+	private final void jdMethod_this() {
+		contextPath = "";
+	}
 
-            localHttpSession.setMaxInactiveInterval(i);
-            localHttpSession.setAttribute("nickname", str1);
-            
+	public void doGet(HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse)
+			throws javax.servlet.ServletException, java.io.IOException {
+		contextPath = paramHttpServletRequest.getContextPath();
+		paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp");
+	}
 
-            krishna.bean.ChatRoom localChatRoom = localChatRoomList.getRoom("StartUp");
-            str1 = str1.toLowerCase();
-            krishna.bean.Chatter localChatter = null;
-      
-            localChatter = new krishna.bean.Chatter(str1, new java.util.Date().getTime());
-            localChatRoom.addChatter(localChatter);
-            paramHttpServletResponse.sendRedirect(contextPath + "/chat.jsp");
-          }
-          
-        }
-        catch (Exception localException)
-        {
-          System.out.println("Exception thrown in LoginServlet: " + localException.getMessage());
-          localException.printStackTrace();
-          paramHttpServletResponse.sendRedirect(contextPath + "/error.jsp");
-        }
-        
-      }
-      else {
-        paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp?ic=t");
-      }
-    }
-  }
+	public void doPost(HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse)
+			throws javax.servlet.ServletException, java.io.IOException {
+		contextPath = paramHttpServletRequest.getContextPath();
+		String str1 = paramHttpServletRequest.getParameter("nickname");
+		str1 = str1.trim().toLowerCase();
+		if ((str1 != null) && (str1.length() > 3) && (str1.indexOf(" ") == -1)) {
+			try {
+				ChatRoomList localChatRoomList = (ChatRoomList) getServletContext().getAttribute("chatroomlist");
+				boolean bool = localChatRoomList.chatterExists(str1);
+				if (bool) {
+					paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp?d=t&n=" + str1);
+				} else {
+					HttpSession localHttpSession = paramHttpServletRequest.getSession(true);
+					int i = 1800;
+					String str3 = getServletContext().getInitParameter("sessionTimeout");
+					if (str3 != null) {
+						try {
+							i = Integer.parseInt(str3);
+							i *= 60;
+						} catch (NumberFormatException localNumberFormatException) {
+						}
+					}
+
+					localHttpSession.setMaxInactiveInterval(i);
+					localHttpSession.setAttribute("nickname", str1);
+
+					krishna.bean.ChatRoom localChatRoom = localChatRoomList.getRoom("StartUp");
+					str1 = str1.toLowerCase();
+					krishna.bean.Chatter localChatter = null;
+
+					localChatter = new krishna.bean.Chatter(str1, new java.util.Date().getTime());
+					localChatRoom.addChatter(localChatter);
+					paramHttpServletResponse.sendRedirect(contextPath + "/chat.jsp");
+				}
+
+			} catch (Exception localException) {
+				System.out.println("Exception thrown in LoginServlet: " + localException.getMessage());
+				localException.printStackTrace();
+				paramHttpServletResponse.sendRedirect(contextPath + "/error.jsp");
+			}
+
+		} else {
+			paramHttpServletResponse.sendRedirect(contextPath + "/login.jsp?ic=t");
+		}
+	}
+}
